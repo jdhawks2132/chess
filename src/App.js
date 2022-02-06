66c57504d4b6d9e047f2286d4d4bc99e4af30ff8
Joshua Hawks <jdhawks@gmail.com>
2022-02-06 09:02:18 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import ChessBoard from 'chessboardjsx';
+import { useState, useEffect, useRef } from 'react';
+import Chess from 'chess.js';
+
+const container = {
+	marginTop: '2rem',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-around',
+	alignItems: 'center',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [fen, setFen] = useState('start');
+
+	let game = useRef(null);
+
+	useEffect(() => {
+		game.current = new Chess();
+	}, []);
+
+	console.log(game);
+
+	const handleOnDrop = ({ sourceSquare, targetSquare }) => {
+		let move = game.current.move({
+			from: sourceSquare,
+			to: targetSquare,
+		});
+		//check for illegal moves
+		if (move === null) return;
+
+		//update fen string
+		setFen(game.current.fen());
+	};
+
+	const handleClickReset = () => {
+		game.current.clear();
+		game.current.reset();
+		setFen('start');
+	};
+
+	return (
+		<div className='App' style={container}>
+			{game.current && game.current.game_over() ? (
+				<div>
+					<h1>Game Over</h1>
+				</div>
+			) : (
+				<span></span>
+			)}
+			<ChessBoard position={fen} onDrop={handleOnDrop} />
+			<div>
+				<button className='btn' onClick={handleClickReset}>
+					reset
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
